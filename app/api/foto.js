4977af4f45772476e1977a4f0be437a7d413857c
Yuri Gomes <yuurig@gmail.com>
2017-02-04 20:41:45 -0200

    Adicionando foto na api

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,27 +1,42 @@
 var api = {};
-	var fotos = [
-		{_id: 1, titulo: 'Leão', url:'http://www.fundosanimais.com/Minis/leoes.jpg' },
-		{_id: 2, titulo: 'Leão 2', url:'http://www.fundosanimais.com/Minis/leoes.jpg' }
-	];
+var contadorId = 2 ;
+var fotos = [{
+		_id: 1,
+		titulo: 'Leão',
+		url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+	},
+	{
+		_id: 2,
+		titulo: 'Leão 2',
+		url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+	}
+];
 
 
-api.lista = function(req, res) {
-	
+api.lista = function (req, res) {
+
 	res.json(fotos);
 }
-api.buscaId = function(req, res) {
-	var foto = fotos.find(function(foto) {
+api.buscaId = function (req, res) {
+	var foto = fotos.find(function (foto) {
 		return foto._id == req.params.id;
 	});
 
 	res.json(foto);
 }
 
-api.removeId = function(req, res) {
-	fotos = fotos.filter(function(foto) {
+api.removeId = function (req, res) {
+	fotos = fotos.filter(function (foto) {
 		return foto._id != req.params.id;
 	});
 
 	res.sendStatus(204);
+}
+
+api.adiciona = function (req, res) {
+	var foto = req.body;
+	foto._id = contadorId + 1 ;
+	fotos.push(foto);
+	res.json(foto);
 }
 module.exports = api;
--- config/express.js
+++ config/express.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('json spaces', 4);
 
+
 app.use(express.static('./public'));
+app.use(bodyParser.json());
 
 consign({cwd: 'app', verbose: false})
 	.include('api')
